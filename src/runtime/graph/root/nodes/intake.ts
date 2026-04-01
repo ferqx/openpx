48e073cf20e9b1dde4ef274aaac484cfb254b29d
chenchao <291180782@qq.com>
2026-04-01 23:29:09 +0800

    Revert "feat: add langgraph root runtime and worker graphs"
    This reverts commit a08d1874bab5c6b637dfc49ca2fad232eddec224.

--- src/runtime/graph/root/nodes/intake.ts
+++
@@ -1,5 +0,0 @@
-export function intakeNode(state: { input: string }) {
-  return {
-    input: state.input.trim(),
-  };
-}
--- src/runtime/graph/root/state.ts
+++
@@ -1,9 +0,0 @@
-import { Annotation, MessagesAnnotation } from "@langchain/langgraph";
-import type { RootMode } from "./context";
-
-export const RootState = Annotation.Root({
-  input: Annotation<string>(),
-  mode: Annotation<RootMode>(),
-  summary: Annotation<string>(),
-  messages: MessagesAnnotation.spec.messages,
-});
